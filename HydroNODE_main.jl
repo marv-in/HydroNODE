7f1d0624a83d8012d405871964dbe5ba277c4d29
marv-in <56080046+marv-in@users.noreply.github.com>
2022-08-21 11:22:48 +0200

    Add files via upload
    Add code files

---
+++ HydroNODE_main.jl
@@ -0,0 +1,203 @@
+# --------------------------------------------------
+# Neural ODE models in Hydrology
+# - load data
+# - build models
+# - train models
+#
+# marvin.hoege@eawag.ch, Mar. 2022
+# --------------------------------------------------
+
+cd(@__DIR__)
+using Pkg; Pkg.activate("."); Pkg.instantiate()
+
+using Revise
+
+using DataFrames, Dates, Statistics
+using DelimitedFiles, CSV, JLD
+
+using OrdinaryDiffEq, DiffEqFlux, Flux
+using DiffEqSensitivity
+
+using GalacticOptim, BlackBoxOptim
+using ForwardDiff, Zygote
+
+using Interpolations
+using Interpolations: interpolate
+
+using Random
+Random.seed!(123)
+
+
+# ===========================================================
+# USER INPUT:
+
+# set data directory
+data_path = joinpath(pwd(),"basin_dataset_public_v1p2")
+
+# choose model M50 or M100
+chosen_model_id = "M50"
+
+# choose basin id
+basin_id = "01013500"
+
+# define training and testing period
+train_start_date = Date(1980,10,01)
+train_stop_date = Date(1981,09,30)
+test_start_date = Date(1981,10,01)
+test_stop_date = Date(2010,09,30)
+
+
+# if `false`, read the bucket model (M0) parameters from "bucket_opt_init.csv"
+train_bucket_model = false
+
+# ===========================================================
+
+includet("HydroNODE_data.jl")
+includet("HydroNODE_models.jl")
+includet("HydroNODE_training.jl")
+
+
+# -------------------------------------------------------
+# Objective function: Nash-Sutcliffe Efficiency
+
+NSE(pred, obs) = 1 - sum((pred .- obs).^2) / sum((obs .- mean(obs)).^2)
+
+function NSE_loss(pred_model, params, batch, time_batch)
+
+    pred, = pred_model(params, time_batch)
+    loss = -NSE(pred,batch)
+
+    return loss, pred
+end
+
+# -------------------------------------------------------
+# Load and preprocess data
+
+input_var_names = ["Daylight(h)", "Prec(mm/day)", "Tmean(C)"]
+output_var_name = "Flow(mm/s)"
+
+df = load_data(lpad(string(basin_id), 8, "0"), data_path)
+
+# drop unused cols
+select!(df, Not(Symbol("SWE(mm)")));
+select!(df, Not(Symbol("Tmax(C)")));
+select!(df, Not(Symbol("Tmin(C)")));
+
+# adjust start and stop date if necessary
+if df[1, "Date"] != train_start_date
+    train_start_date = maximum([df[1, "Date"],train_start_date])
+end
+
+if df[end, "Date"] != test_stop_date
+    test_stop_date = minimum([df[end, "Date"], test_stop_date])
+end
+
+# format data
+data_x, data_y, data_timepoints,
+train_x, train_y, train_timepoints, = prepare_data(df,
+(train_start_date, train_stop_date, test_start_date, test_stop_date),input_var_names,output_var_name)
+
+# normalize data
+norm_moments_in = [mean(data_x, dims=1); std(data_x, dims = 1)]
+
+norm_P = prep_norm(norm_moments_in[:,2])
+norm_T = prep_norm(norm_moments_in[:,3])
+
+# -------------------------------------------------------
+# interpolation
+
+itp_method = SteffenMonotonicInterpolation()
+
+itp_Lday = interpolate(data_timepoints, data_x[:,1], itp_method)
+itp_P = interpolate(data_timepoints, data_x[:,2], itp_method)
+itp_T = interpolate(data_timepoints, data_x[:,3], itp_method)
+
+# ===============================================================
+# Bucket model training and full model preparation
+
+NSE_loss_bucket_w_states(p) =  NSE_loss(basic_bucket_incl_states, p, train_y, train_timepoints)[1]
+
+@info "Bucket model training..."
+
+# Parameter ranges for bucket model:
+# f: Rate of decline in flow from catchment bucket   | Range: (0, 0.1)
+# smax: Maximum storage of the catchment bucket      | Range: (100, 1500)
+# qmax: Maximum subsurface flow at full bucket       | Range: (10, 50)
+# ddf: Thermal degree‐day factor                     | Range: (0, 5.0)
+# tmax: Temperature above which snow starts melting  | Range: (0, 3.0)
+# tmin: Temperature below which precipitation is snow| Range: (-3.0, 0)
+
+
+if train_bucket_model
+    lower_bounds = [0.01, 100.0, 0.0, 100.0, 10.0, 0.01, 0.0, -3.0]
+    upper_bounds = [1500.0, 1500.0, 0.1, 1500.0, 50.0, 5.0, 3.0, 0.0]
+
+    SearchRange = [(lower_bounds[i], upper_bounds[i]) for i in 1:length(lower_bounds)]
+
+    p_all_opt_bucket = best_candidate(BlackBoxOptim.bboptimize(NSE_loss_bucket_w_states; SearchRange, MaxSteps = 5000))
+
+
+    S_bucket_precalib = p_all_opt_bucket[1:2]
+    p_bucket_precalib = p_all_opt_bucket[3:end]
+else
+
+    bucket_opt_init = readdlm("bucket_opt_init.csv", ',')
+    basins_available = lpad.(string.(Int.(bucket_opt_init[:,1])), 8, "0")
+
+    basin_wanted = findall(x -> x==basin_id, basins_available)[1]
+
+    p_all_opt_bucket = bucket_opt_init[basin_wanted, 2:end]
+
+    S_bucket_precalib = p_all_opt_bucket[1:2]
+    p_bucket_precalib = p_all_opt_bucket[3:end]
+end
+@info "... complete!"
+
+Q_bucket, S_bucket  = basic_bucket_incl_states([S_bucket_precalib..., p_bucket_precalib...], train_timepoints)
+
+NSE_opt_bucket = -NSE_loss_bucket_w_states(p_all_opt_bucket)
+
+
+# ===============================================================
+# Neural ODE models
+
+# -------------
+# preparation
+
+norm_S0 = prep_norm([mean(S_bucket[1,:]), std(S_bucket[1,:])])
+norm_S1 = prep_norm([mean(S_bucket[2,:]), std(S_bucket[2,:])])
+
+NN_NODE, p_NN_init = initialize_NN_model(chosen_model_id)
+
+S0_bucket_ = S_bucket[1,:]
+S1_bucket_ = S_bucket[2,:]
+Lday_bucket_ = train_x[:,1]
+P_bucket_ = train_x[:,2]
+T_bucket_ = train_x[:,3]
+
+NN_input = [norm_S0.(S0_bucket_) norm_S1.(S1_bucket_) norm_P.(P_bucket_) norm_T.(T_bucket_)]
+
+@info "NN pre-training..."
+p_NN_init = pretrain_NNs_for_bucket_processes(chosen_model_id, NN_NODE, p_NN_init,
+    NN_input, p_bucket_precalib, S0_bucket_, S1_bucket_, Lday_bucket_, P_bucket_, T_bucket_;
+    print_results=false)
+@info "... complete!"
+
+pred_NODE_model= prep_pred_NODE(NN_NODE, p_bucket_precalib[6:-1:4], S_bucket_precalib, length.(initial_params.(NN_NODE)))
+
+# -------------
+# training
+
+@info "Neural ODE model training..."
+p_opt_NODE = train_model(pred_NODE_model, p_NN_init, train_y, train_timepoints; optmzr = ADAM(0.001), max_N_iter = 75)
+@info "... complete."
+
+NSE_opt_NODE = -NSE_loss(pred_NODE_model,p_opt_NODE, train_y, train_timepoints)[1]
+
+# -------------
+# comparison bucket vs. Neural ODE
+@info "Nash-Sutcliffe-Efficiency comparison (optimal value: 1):"
+
+@info "NSE bucket model: $NSE_opt_bucket"
+
+@info "NSE NeuralODE model: $NSE_opt_NODE"
---
+++ HydroNODE_training.jl
@@ -0,0 +1,127 @@
+# --------------------------------------------------
+# HydroNODE Model Training
+#
+# pre-train NNs for single processes to make them learn the basic
+# physical relations between input variables and process output
+#
+# marvin.hoege@eawag.ch, Mar. 2022
+# --------------------------------------------------
+
+function prep_norm(norm_moments)
+  val -> (val .- norm_moments[1])./norm_moments[2]
+end
+
+
+function pretrain_submodel(model, input, p_init, target_data; optmzr = ADAM(0.01), max_N_iter = 300)
+  # pretrain NN via sum of least squares
+
+  function prep_pred_NN_pretrain(model_, input_)
+      (params) -> model_(input_,params)
+  end
+
+  pred_NN_pretrain_fct = prep_pred_NN_pretrain(model, permutedims(input))
+
+  function loss_NN_pretrain(params, batch)
+      sum((pred_NN_pretrain_fct(params)'.-batch).^2)
+  end
+
+  optf = GalacticOptim.OptimizationFunction((θ, p) -> loss_NN_pretrain(θ, target_data), GalacticOptim.AutoZygote())
+  optprob = GalacticOptim.OptimizationProblem(optf, p_init)
+  sol = GalacticOptim.solve(optprob, optmzr, maxiters = max_N_iter)
+
+  pred_opt = pred_NN_pretrain_fct(sol)'
+
+  return sol, pred_opt
+
+end
+
+
+function pretrain_NNs_for_bucket_processes(chosen_model_id, NNs_in_fct, p_NNs_init, NN_input, p_bucket,S0_bucket, S1_bucket, Lday_bucket, P_bucket, T_bucket; print_results=false)
+
+  f_bucket    = p_bucket[1]
+  Smax_bucket = p_bucket[2]
+  Qmax_bucket = p_bucket[3]
+  Df_bucket   = p_bucket[4]
+  Tmax_bucket = p_bucket[5]
+  Tmin_bucket = p_bucket[6]
+
+  # evapotranspiration
+  ET_mech = ET.(S1_bucket, T_bucket, Lday_bucket, Smax_bucket)
+  ET_mech = [x<0.0 ? 0.000000001 : x for x in ET_mech]
+
+  # discharge
+  Q_mech = Qb.(S1_bucket,f_bucket,Smax_bucket,Qmax_bucket) .+ Qs.(S1_bucket, Smax_bucket)
+  Q_mech = [x<0.0 ? 0.000000001 : x for x in Q_mech]
+
+  # snow precipitation
+  Ps_mech = Ps.(P_bucket, T_bucket, Tmin_bucket)
+
+  # rain precipitation
+  Pr_mech = Pr.(P_bucket, T_bucket, Tmin_bucket)
+
+
+  if chosen_model_id == "M50"
+    NN_in_fct101 = NNs_in_fct[1]
+    p_NN_init_101 = p_NNs_init[1]
+
+    NN_in_fct102 = NNs_in_fct[2]
+    p_NN_init_102 = p_NNs_init[2]
+
+    # NN for ET
+    p_NN_precal_101, = pretrain_submodel(NN_in_fct101, NN_input[:, [1,2,4]], p_NN_init_101,
+      log.(ET_mech./Lday_bucket);
+      optmzr = ADAM(0.01), max_N_iter = 300)
+
+    # NN for Q
+    p_NN_precal_102, = pretrain_submodel(NN_in_fct102, NN_input[:, [2,3]], p_NN_init_102,
+      log.(Q_mech);
+      optmzr = ADAM(0.01), max_N_iter = 300)
+
+    p_NN_init_new = [p_NN_precal_101..., p_NN_precal_102...]
+
+
+  elseif chosen_model_id == "M100"
+
+    M_mech = minimum(hcat(S0_bucket, Df_bucket*(T_bucket.-Tmax_bucket)), dims = 2) .* step_fct.(T_bucket.-Tmax_bucket)
+    M_mech = [x<0.0 ? 0.0 : x for x in M_mech]
+
+    NN_in_fct101 = NNs_in_fct
+    p_NN_init_101 = p_NNs_init
+
+    p_NN_precal_101, = pretrain_submodel(NN_in_fct101, NN_input[:, [1,2,3,4]], p_NN_init_101,
+    [log.(ET_mech./Lday_bucket) log.(Q_mech) asinh.(M_mech) asinh.(Ps_mech) asinh.(Pr_mech)];
+    optmzr = ADAM(0.01), max_N_iter = 1000)
+
+    p_NN_init_new = p_NN_precal_101
+
+  end
+
+  return p_NN_init_new
+
+end
+
+
+function train_model(pred_NODE, p_init, target_data, target_time; optmzr = ADAM(0.01), max_N_iter = 75)
+
+  function prep_pred_model(time_batch)
+      (p) -> pred_NODE(p, time_batch)[1]
+  end
+
+  pred_NN_model_fct = prep_pred_model(target_time)
+
+  function loss_model(p)
+      -NSE(pred_NN_model_fct(p),target_data)
+  end
+
+  callback = function (p,l)
+      println("NSE: "*string(-l))
+      return false
+  end
+
+  optf = GalacticOptim.OptimizationFunction((θ, p) -> loss_model(θ), GalacticOptim.AutoZygote())
+  optprob = GalacticOptim.OptimizationProblem(optf, p_init)
+  sol = GalacticOptim.solve(optprob, optmzr, cb = callback, maxiters = max_N_iter)
+
+  return sol
+
+end
